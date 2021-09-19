b19b830d01f0e9a5cbf0da064bd1bca5599112e8
Pablo Aramburo <josepablo.aramburo@gmail.com>
2021-09-19 02:15:06 -0600

    [mod] Valid env names

--- index.py
+++ index.py
@@ -28,8 +28,8 @@ class handler(BaseHTTPRequestHandler):
         # Sending the code over sms with the aws sns
         sns = boto3.resource(
             'sns',
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
+            aws_access_key_id=os.getenv('MFA_AWS_ACCESS_KEY'),
+            aws_secret_access_key=os.getenv('MFA_AWS_SECRET_KEY'),
             region_name='us-east-1'
         )
 
